f82080fb4af607171f3f06360df7643d80e6cf0a
Wilku <arek.szumacher@gmail.com>
2021-11-03 15:25:00 +0100

    new

--- webConnectChecker.py
+++ webConnectChecker.py
@@ -1,9 +1,9 @@
 import os
 import time
+from subprocess import Popen
 
 hostname = "google.com"
 counter = 0
-
 
 while True:
     response = os.system("ping -c 1 " + hostname)
@@ -14,10 +14,12 @@ while True:
     else:
         if counter < 5:
             #TODO turn off radio
+            Process=Popen('/home/pi/Desktop/radio/k1.sh %s' % (str(0)), shell=True)
             time.sleep(20)
             #TODO turn on radio
             counter += 1
-            print(hostname, 'is down!')
+            Process=Popen('/home/pi/Desktop/radio/k1.sh %s' % (str(1)), shell=True)
+            time.sleep(60)
         else:
             counter = 0
             time.sleep(3600)
