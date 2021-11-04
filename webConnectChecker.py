4abf9d5474e109a01257f851ca03775cedea2001
A.Szumacher <a.szumacher@boosthigh.com>
2021-11-04 07:12:49 +0100

    remove comments

--- webConnectChecker.py
+++ webConnectChecker.py
@@ -7,16 +7,12 @@ counter = 0
 
 while True:
     response = os.system("ping -c 1 " + hostname)
-
-    #and then check the response...
     if response == 0:
         time.sleep(60)
     else:
         if counter < 5:
-            #TODO turn off radio
             Process=Popen('/home/pi/Desktop/radio/k1.sh %s' % (str(0)), shell=True)
             time.sleep(20)
-            #TODO turn on radio
             counter += 1
             Process=Popen('/home/pi/Desktop/radio/k1.sh %s' % (str(1)), shell=True)
             time.sleep(60)
