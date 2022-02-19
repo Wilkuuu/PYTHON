a050f5b99796b02c65ec7be3bdbcc1d6b4cea6a6
Wilku <arek.szumacher@gmail.com>
2022-02-19 17:21:21 +0100

    refactor

--- webConnectChecker.py
+++ webConnectChecker.py
@@ -8,17 +8,17 @@ counter = 0
 while True:
     response = os.system("ping -c 1 " + hostname)
 
-    #and then check the response...
+    # and then check the response...
     if response == 0:
         time.sleep(60)
     else:
         if counter < 5:
-            #TODO turn off radio
-            Process=Popen('/home/pi/Desktop/radio/k1.sh %s' % (str(0)), shell=True)
+            # TODO turn off radio
+            Process = Popen('/home/pi/Desktop/radio/k1.sh %s' % (str(0)), shell=True)
             time.sleep(20)
-            #TODO turn on radio
+            # TODO turn on radio
             counter += 1
-            Process=Popen('/home/pi/Desktop/radio/k1.sh %s' % (str(1)), shell=True)
+            Process = Popen('/home/pi/Desktop/radio/k1.sh %s' % (str(1)), shell=True)
             time.sleep(60)
         else:
             counter = 0
